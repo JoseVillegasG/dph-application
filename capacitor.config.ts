7a1a9019fe1814ee5970cc5da4a1bb07a7d4972c
JoseVillegasG <jose.villegas@cetys.edu.mx>
2025-03-12 23:05:52 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DPHApplication',
+  webDir: 'dist'
+};
+
+export default config;
